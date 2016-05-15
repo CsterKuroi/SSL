5c6f23f50b53fd2dfe6f5acc797de0af3aafd7e2
Kuroi <412681778@qq.com>
2016-05-15 15:08:16 +0800

    ssl demo

---
+++ MyWebServer.cpp
@@ -0,0 +1,25 @@
+#include "common.h"
+#include "HttpProtocol.h"
+
+int main()
+{
+	CHttpProtocol MyHttpObj;
+	MyHttpObj.StartHttpSrv();
+	SSL *ssl;
+	BYTE buf[4096];
+	BIO *io;
+	bool bRet;
+	bRet = MyHttpObj.SSLRecvRequest(ssl,io,buf,sizeof(buf));
+	if(!bRet)
+		{
+			char tip[] = "Receiving request error! \n";
+			MyHttpObj.err_exit(tip);
+		}
+		else
+			{
+				printf("Request received!! \n");
+				printf("%s \n",buf);
+			}
+  sleep(1000);
+	return 0;
+}
